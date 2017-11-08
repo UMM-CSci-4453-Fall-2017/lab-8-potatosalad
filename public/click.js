0f01a387bd686622f7ba4323c2dee717e8ed09bb
Kyle M Foss <fossx229@neoprene.morris.umn.edu>
2017-11-08 17:56:07 -0600

    --We added stuff for the items and buttons, so they work with express and the database.

--- public/click.js
+++ public/click.js
@@ -1,10 +1,11 @@
-angular.module('buttons',[])
-  .controller('buttonCtrl',ButtonCtrl)
-  .factory('buttonApi',buttonApi)
+angular.module('register',[])
+  .controller('registerCtrl',registerCtrl)
+  .factory('registerApi',registerApi)
   .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
-function ButtonCtrl($scope,buttonApi){
+function registerCtrl($scope,registerApi){
    $scope.buttons=[]; //Initially all was still
+   $scope.items=[];
    $scope.errorMessage='';
    $scope.isLoading=isLoading;
    $scope.refreshButtons=refreshButtons;
@@ -18,10 +19,11 @@ function ButtonCtrl($scope,buttonApi){
   function refreshButtons(){
     loading=true;
     $scope.errorMessage='';
-    buttonApi.getButtons()
+    registerApi.getButtons()
       .success(function(data){
          $scope.buttons=data;
          loading=false;
+         console.log(items);
       })
       .error(function () {
           $scope.errorMessage="Unable to load Buttons:  Database request failed";
@@ -30,25 +32,39 @@ function ButtonCtrl($scope,buttonApi){
  }
   function buttonClick($event){
      $scope.errorMessage='';
-     buttonApi.clickButton($event.target.id)
+     registerApi.clickButton($event.target.id)
         .success(function(){})
         .error(function(){$scope.errorMessage="Unable click";});
   }
   refreshButtons();  //make sure the buttons are loaded
 
-}
 
-function buttonApi($http,apiUrl){
+
+function itemClick($event){
+  $scope.errorMessage='';
+  registerApi.clickItem($event.target.id)
+    .success(function(){registerApi.getItems()})
+    .error(function(){$scope.errorMessage="Error clicking on item -- Can't Delete!"})
+}
+}
+function registerApi($http,apiUrl){
   return{
     getButtons: function(){
       var url = apiUrl + '/buttons';
       return $http.get(url);
     },
+    getItems: function(){
+      var url = apiUrl + '/items';
+      return $http.get(url)
+    },
     clickButton: function(id){
       var url = apiUrl+'/click?id='+id;
 //      console.log("Attempting with "+url);
       return $http.get(url); // Easy enough to do this way
+    },
+    clickItem: function(id){
+      var url = apiUrl +'/itemclick?id='+id
+      return $http.get(url);
     }
  };
 }
-
