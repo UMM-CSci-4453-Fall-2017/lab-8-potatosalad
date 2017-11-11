aadaf55e1dd2aacf3d6c79346e71d3284151d809
Kyle M Foss <fossx229@neoprene.morris.umn.edu>
2017-11-10 20:58:33 -0600

    added documentation

--- express.js
+++ express.js
@@ -5,7 +5,6 @@ credentials=require('./resources/credentials.json'),
 DBF=require('./resources/dbf-setup.js');
 var user = credentials.user;
 var buttonInfo = [];
-// var transactionItems = [];
 
 app = express(),
 port = process.env.PORT || 1337;
@@ -14,96 +13,71 @@ credentials.host='ids.morris.umn.edu'; //setup database credentials
 
 var connection = mysql.createConnection(credentials); // setup the connection
 
-connection.connect(function(err){if(err){console.log(error)}});
+connection.connect(function(err){if(err){console.log(error)}}); // Connect to the database
 
+app.use(express.static(__dirname + '/public')); // Tells express to use the files in the public folder
 
-app.use(express.static(__dirname + '/public'));
+// Server endpoint for handling buttons. Upon request, will query the till_buttons table in the database and send the table information to the controller. Otherwise an error will be sent.
 app.get("/buttons",function(req,res){
   var sql = 'SELECT till_buttons.*, prices.price FROM ' + user + '.till_buttons INNER JOIN '+ user + '.prices ON till_buttons.id = prices.id';
   connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an error:");
-             console.log(err);}
-             buttonInfo = rows;
-             console.log(buttonInfo);
-     res.send(rows);
+    if(err){console.log("We have an error:");
+    console.log(err);}
+    buttonInfo = rows;
+    res.send(rows);
   }})(res));
 });
 
+// Server endpoint for getting items for the controller. When a request is made, it will query the database for the current transaction items, and will send this to the controller. Otherwise an Error.
 app.get("/items", function(req, res){
   var sql ='SELECT * FROM ' + user +'.transaction';
   connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an error:");
-             console.log(err);}
-        //     transactionItems = rows;
-        //     console.log(transactionItems);
-     res.send(rows);
+    if(err){console.log("We have an error:");
+    console.log(err);}
+    //     transactionItems = rows;
+    //     console.log(transactionItems);
+    res.send(rows);
   }})(res));
 });
 
+// Server endpoint for handling the event where an item is clicked on the screen. Will query the database to delete the item from the transaction table which corresponds to what was clicked.
+// An empty response will be sent to the controller, otherwise an error.
 app.get("/itemclick", function(req, res){
-  console.log("Doing stuff");
   var id = req.param("id");
-  console.log(id);
-  console.log(typeof(id));
   var sql = 'DELETE FROM ' + user + '.transaction WHERE itemID = ' + id+';';
-  console.log(sql);
   connection.query(sql,(function(res){return function(err,rows,fields){
-    console.log("here");
     //throws errors when clicked but deletes the right buttons when refreshed.
-    /*if(err){console.log("We have an insertion error:");
-            console.log(err);
-            res.send(err);
-    }
-    */
-
-    res.send();
-  }})(res));
+    if(err){console.log("We have an insertion error:");
+    console.log(err);
+    res.send(err);
+  }
+  res.send();
+}})(res));
 });
 
-
+// Server endpoint for handling button clicks. On a button click, another item that corresponds to the clicked button will be inserted into the database. If there is already an item of that
+// type in the transaction table, the quantity of that item will be incremented. Will send an empty response to the controller, otherwise an error.
 app.get("/click",function(req,res){
   var id = req.param("id");
-  console.log(id);
+
+  // Extracts the item information that is stored along with the buttons in the server button array so that the insert statement is complete
   var label = extractProperty(buttonInfo, "label", id);
   var price = extractProperty(buttonInfo, "price", id);
-  console.log(label);
-  console.log(price);
 
   var sql = "INSERT INTO " + user + ".transaction VALUES (" + 01 + ", " + id + ", '" + label + "', " + 1 + ", " + price + ") ON DUPLICATE KEY UPDATE quantity = quantity + 1";
 
-  /*
-  if (extractProperty(transactionItems, "id", id) == -1) {
-  var transactionItem = {
-    item: label,
-    price: price,
-    quantity: 1,
-    id: id
-  };
-  transactionItems.push(transactionItem);
-}
-else {
-  for(var j = 0; j < transactionItems.length; j++) {
-    console.log(transactionItems.length);
-    console.log(transactionItems);
-    if(transactionItems[j].id==id) {
-      transactionItems[j].quantity = transactionItems[j].quantity + 1;
-      sql = "UPDATE " + user + ".transaction SET quantity = " + extractProperty(transactionItems, "quantity", id) + " WHERE itemID = " + id;
-    }
-  }
-}
-*/
-console.log("Attempting sql ->"+sql+"<-");
   connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);
-             res.send(err);
-           }
+    if(err){console.log("We have an insertion error:");
+    console.log(err);
+    res.send(err);
+  }
 
-  }})(res));
-  res.send();
+}})(res));
+res.send();
 });
 // Your other API handlers go here!
 
+// Helper function for getting information from our button array for formatting our insert statements. We wrote it because we couldn't seem to get the map function to work...
 function extractProperty (array, propertyName, id) {
   for (var i = 0; i < array.length; i++) {
     if (array[i].id == id) {
--- public/click.js
+++ public/click.js
@@ -1,97 +1,97 @@
 angular.module('register',[])
-  .controller('registerCtrl',registerCtrl)
-  .factory('registerApi',registerApi)
-  .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
+.controller('registerCtrl',registerCtrl)
+.factory('registerApi',registerApi)
+.constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
 function registerCtrl($scope,registerApi){
-   $scope.buttons=[]; //Initially all was still
-   $scope.items=[];
-   $scope.total=function(items) {
+  $scope.buttons=[]; //Initially all was still
+  $scope.items=[];  // Also empty initally
+  $scope.total=function(items) {
     var total = 0;
-    parseInt(total);
-     for (var i = 0; i < items.length; i++) {
-       total = parseInt(total) + parseInt(findSum(items[i].price, items[i].quantity));
-     }
-     return total.toFixed(2);
-   };
-   $scope.errorMessage='';
-   $scope.isLoading=isLoading;
-   $scope.refreshButtons=refreshButtons;
-   $scope.buttonClick=buttonClick;
-   $scope.findSum=function(price, quantity) {
-     return (price * quantity).toFixed(2);
-   }
-   $scope.itemClick=itemClick;
+    // converts total from a string to a float
+    parseFloat(total).toFixed(2);
+    for (var i = 0; i < items.length; i++) {
+      total = parseFloat(total) + parseFloat(findSum(items[i].price, items[i].quantity));
+    }
+    return total.toFixed(2);
+  };
+  // Defining which functions are visible in the HTML
+  $scope.errorMessage='';
+  $scope.isLoading=isLoading;
+  $scope.refreshButtons=refreshButtons;
+  $scope.buttonClick=buttonClick;
+  $scope.findSum=findSum;
+  $scope.itemClick=itemClick;
 
-   var loading = false;
-
-   function isLoading(){
+  var loading = false;
+  function isLoading(){
     return loading;
-   }
+  }
 
+  // Sends a request to the server for button information. Upon a successfully recieved response, the controllers button array will become the new information from the server
+  // Otherwise a error will be shown and loading will be set to false.
   function refreshButtons(){
     loading=true;
     $scope.errorMessage='';
     registerApi.getButtons()
-      .success(function(data){
-         $scope.buttons=data;
-         loading=false;
-         console.log(items);
-      })
-      .error(function () {
-          $scope.errorMessage="Unable to load Buttons:  Database request failed";
-          loading=false;
-      });
- }
+    .success(function(data){
+      $scope.buttons=data;
+      loading=false;
+      console.log(items);
+    })
+    .error(function () {
+      $scope.errorMessage="Unable to load Buttons:  Database request failed";
+      loading=false;
+    });
+  }
+  // Sends a request to the server for item information. Upon a successfully recieved response, the controllers item array will become the new information from the server
+  // Otherwise a error will be shown and loading will be set to false.
   function retrieveItems(){
     loading = true;
     $scope.errorMessage='';
     registerApi.getItems()
-       .success(function(data){
-         $scope.items = data;
-         loading=false;
+    .success(function(data){
+      $scope.items = data;
+      loading=false;
+    }
+  )
+  .error(function(){$scope.errorMessage="Unable click"; loading=false;});
+}
 
-       }
-     )
-       .error(function(){$scope.errorMessage="Unable click"; loading=false;});
- }
+// Sends a request to the server that indicates a button has been clicked. Upon a successfully recieved response, the controller will update its button and item arrays
+// from the server. Otherwise a error will be shown and loading will be set to false.
+function buttonClick($event){
+  $scope.errorMessage='';
+  registerApi.clickButton($event.target.id)
+  .success(function(data){
+    retrieveItems();
+    refreshButtons();
+  })
+  .error(function(){$scope.errorMessage="Unable click";});
 
+}
+refreshButtons();  //makes sure the buttons are loaded
+retrieveItems();  // makes sure the items are loaded
 
-
-  function buttonClick($event){
-     $scope.errorMessage='';
-     registerApi.clickButton($event.target.id)
-        .success(function(data){
-          $scope.buttons = data;
-          retrieveItems();
-          refreshButtons();
-        })
-        .error(function(){$scope.errorMessage="Unable click";});
-
-  }
-  refreshButtons();  //make sure the buttons are loaded
-  retrieveItems();
-
+// Tells the server that an item has been pressed and includes the corresponding id of the item. Upon a successfully recieved response, the controllers item array will be
+// updated from the server. Otherwise a error will be shown
 function itemClick(id){
   $scope.errorMessage='';
   registerApi.clickItem(id)
-    .success(function(){
-      console.log("Hello");
-      retrieveItems();
-    })
-    .error(function(){$scope.errorMessage="Error clicking on item -- Can't Delete!"});
-    //retrieveItems();
-
+  .success(function(){
+    retrieveItems();
+  })
+  .error(function(){$scope.errorMessage="Error clicking on item -- Can't Delete!"});
 }
 
+// Helper function that multiples an items price by its quantity to provide information about the subtotal of a specific item
 function findSum(price, quantity) {
   return (price * quantity).toFixed(2);
 }
 
-
-
 }
 
+// function that accesses the server API endpoints in the server based on which function is called in this controller
 function registerApi($http,apiUrl){
   return{
     getButtons: function(){
@@ -104,17 +104,11 @@ function registerApi($http,apiUrl){
     },
     clickButton: function(id){
       var url = apiUrl+'/click?id='+id;
-
       return $http.get(url); // Easy enough to do this way
     },
     clickItem: function(id){
-      console.log("we make it here");
-
       var url = apiUrl +'/itemclick?id='+id
-
-         console.log("Attempting with "+url);
-         console.log(typeof(url));
       return $http.get(url);
     }
- };
+  };
 }
